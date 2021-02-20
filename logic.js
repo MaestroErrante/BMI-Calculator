a9ce9ee9c4bdc0ecc9aae63d1727a9a8072e32ee
Rudyard <rudyard90@gmail.com>
2021-02-20 14:52:36 -0600

    Delete default logic

--- logic.js
+++ logic.js
@@ -1,7 +1,2 @@
 "use strict";
-//Default Parameters
-document.querySelector(".weight").value = "";
-
-document.querySelector(".age").value = "";
-
-document.querySelector(".height").value = "";
+//Selecting Elements
