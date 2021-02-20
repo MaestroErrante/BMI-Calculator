3e50d7e3bccd7716c7583908fcdae602ba37c932
Rudyard <rudyard90@gmail.com>
2021-02-20 15:19:11 -0600

    Set default conditions to 0

--- logic.js
+++ logic.js
@@ -6,3 +6,8 @@ const userHeight = document.querySelector(".height");
 const maleBtn = document.querySelector(".btnMale");
 const femaleBtn = document.querySelector(".btnFemale");
 const answerBtn = document.querySelector(".answerButton");
+
+//Starting Conditions
+userWeight.textContent = 0;
+userAge.textContent = 0;
+userHeight.textContent = 0;
