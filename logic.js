fec5c25cd6ac82a894d745e02249f03bc4312cfc
Rudyard <rudyard90@gmail.com>
2021-02-20 15:07:57 -0600

    Selecting items

--- logic.js
+++ logic.js
@@ -1,2 +1,8 @@
 "use strict";
 //Selecting Elements
+const userWeight = document.querySelector(".weight");
+const userAge = document.querySelector(".age");
+const userHeight = document.querySelector(".height");
+const malebtn = document.querySelector(".btnMale");
+const femalebtn = document.querySelector(".btnFemale");
+const answerbtn = document.querySelector(".answerButton");
